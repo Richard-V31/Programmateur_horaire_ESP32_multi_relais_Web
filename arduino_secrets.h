8f4ad3e2c8f9d7f8a27f0cac765cb6082ccb1b07
Richard <richvern31@gmail.com>
2026-08-31 13:16:49 +0200

    Update WiFi credentials in arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,6 +1,8 @@
-#define SECRET_SSID  "Box 1"
-#define SECRET_PASS  "Pass 1"
-#define SECRET_SSID2 "Box 2"
-#define SECRET_PASS2 "Pass 2"
-//#define SECRET_SSID3 "Nouvelle Box"
-//#define SECRET_PASS3 "Mot de passe"
+#define SECRET_SSID  "NomDeVotreReseau1"
+#define SECRET_PASS  "MotDePasse1"
+#define SECRET_SSID2 "NomDeVotreReseau2"
+#define SECRET_PASS2 "MotDePasse2"
+
+// Optionnel : décommentez pour ajouter un 3e réseau connu
+// #define SECRET_SSID3 "NomDeVotreReseau3"
+// #define SECRET_PASS3 "MotDePasse3"
